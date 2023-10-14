2d0125c91736f52d93caf15cea5b35eb52657523
ARUNAGIRI <ASubramanian2@lnw.com>
2023-10-14 20:38:46 +0530

    ToDo Web App

---
+++ app.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Depends, HTTPException, Request, Form, status,Response
+from fastapi.responses import HTMLResponse
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+import uvicorn
+import models
+import database
+from database import engine
+
+models.Base.metadata.create_all(engine)
+
+templates = Jinja2Templates(directory="FastApi/Templates")
+
+app = FastAPI()
+get_db = database.get_db
+
+@app.get("/")
+def home(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(models.Todo).all()
+    return templates.TemplateResponse("base.html",
+                                      {"request": request, "todo_list": todos})
+
+@app.post("/add")
+def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+    new_todo = models.Todo(title=title)
+    db.add(new_todo)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@app.get("/update/{todo_id}")
+def update(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.complete = not todo.complete
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+
+@app.get("/delete/{todo_id}")
+def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
